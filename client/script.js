4f4e0eb7cad3eaf71221ead054f0fe760a19ccef
Anthony <97515439+katastropeee@users.noreply.github.com>
2023-01-08 18:30:30 +0800

    first edit

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async(e) => {
 
      //fetch from backend
 
-     const response = await fetch('http://localhost:5000', {
+     const response = await fetch('https://cody-ai.onrender.com', {
         method: 'post',
         headers: {
             'Content-Type': 'application/json'
